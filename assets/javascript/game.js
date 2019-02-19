2e8a38a474d27d2a6ff3a705058b14933265c7de
Alex Rhinehart <rhinehart.alex05@gmail.com>
2019-02-19 00:01:09 -0500

    updated css, html, and linked javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+//Create Array of the alphabet and words
+var alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var word = ['confused','hardware','resource','tomorrow','firewall','internet'];
+
+
+
+//Computer chooses word randomly
+var random = Math.random(Math.random() * word.length);
+
+//Get guess of player
+
+
+//Create if, else if, else statements based on choice of letter
+
+
+
+//move wrong guess to incorrect
+//if right guess add letter to word
