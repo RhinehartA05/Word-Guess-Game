8044558af8fd4136574fd0685d85f6b9be61a492
Alex Rhinehart <rhinehart.alex05@gmail.com>
2019-02-24 14:44:13 -0500

    updated javascript and html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,46 @@
 //Create Array of the alphabet and words
-var alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-var word = ['confused','hardware','resource','tomorrow','firewall','internet'];
+let alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+let word = ['confused','hardware','resource','tomorrow','firewall','internet'];
+
+
+// let spaces = ['_','_','_','_','_','_','_','_']
+let notGuessed = word.length;
+
+
+
 
 
 
 //Computer chooses word randomly
-var random = Math.random(Math.random() * word.length);
+let random = Math.floor(Math.random() * word.length); //returns a random word
+let blank = word[random] //the random word will be equal to blank
+console.log(blank);
+
+//creating underscore
+let underScore = [];
+for (let i = 0; i < word.length; i++){
+    underScore[i]= "_";
+};
+
+while (notGuessed > 0){
+
+};
+ 
+
+
+// let add = function() { //copy paste
+//     console.log("add stuff"); //copy paste
+// }
 
 //Get guess of player
+document.onkeyup = function(event){
+    var playerGuess = event.key
 
+    if (playerGuess === "r")
+
+    console.log("Your guess: " + playerGuess);
+
+}
 
 //Create if, else if, else statements based on choice of letter
 
